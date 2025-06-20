e5185e2576da34468c9ddb04bd93ced5aca5ea55
Harsh Sharma <krish9515274@gmail.com>
2025-06-20 19:14:38 +0530

    added company route

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
 import userRoute from './routes/user.route.js';
+import companyRoute from './routes/company.route.js';
 dotenv.config({});
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(cors(corsOptions));
 
 //API's
 app.use('/api/v1/user', userRoute);
+app.use('/api/v1/company',companyRoute);
 
 
 app.listen(PORT, () => {
