5b10dcc45c50cd48c99c4a2e3b6ab475df857fc1
Harsh Sharma <krish9515274@gmail.com>
2025-06-21 14:47:37 +0530

    completed application controller

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -79,3 +79,65 @@ export const getAppliedJobs = async(req,res) => {
         console.log(e);
     }
 }
+
+export const getApplicants = async(req,res) => {
+    try{
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId).populate({
+            path: 'applications',
+            options: {sort:{createdAt: -1}},
+            populate:{
+                path: 'applicant'
+            }
+        })
+
+        if(!job){
+            return res.status(400).json({
+                message: 'Job not found',
+                success:false
+            })
+        };
+
+        return res.status(200).json({
+            job,
+            success:true
+        })
+    }
+
+    catch(e){
+        console.log(e);
+    }
+}
+
+export const updateStatus = async(req,res) => {
+    try{
+        const {status} = req.body;
+        const applicationId = req.params.id;
+        if(!status){
+            return res.status(400).json({
+                message: 'status is required',
+                success:false
+            })
+        }
+
+        const application= await Application.findOne({_id:applicationId});
+        if(!application){
+            return res.status(404).json({
+                message: 'Application not found',
+                success: false
+            })
+        }
+
+        application.status = status.toLowerCase();
+        await application.save();
+
+        return res.status(200).json({
+            message: 'Status updated successfully',
+            success:true
+        })
+        
+    }
+    catch(e){
+        console.log(e);
+    }
+}
